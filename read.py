e33818ed397f5a5bc418b31f38ae2bd87ab93caa
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-05-26 17:56:53 +0530

    Update read.py

--- read.py
+++ read.py
@@ -1,6 +1,6 @@
 #This code reads the content of file named as ex1.txt 
-with open("ex1.txt","r")as File1:
-    file_stuff=File1.read()
+with open("ex1.txt","r")as File1: #file ex1.txt is opened as File1
+    file_stuff=File1.read() #content of File1 is stored in the string named as file_stuff
     print(file_stuff)
     print(File1.closed)
     print(file_stuff)
